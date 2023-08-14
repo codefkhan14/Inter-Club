b4207ab725f9fff7f9f7266cd8890dcf6a5cfc1a
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-08-15 01:28:05 +0530

    add server

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000"; // Update this with your actual backend URL
+const API_BASE_URL = "https://interclub.onrender.com/"; // Update this with your actual backend URL
 // const API_BASE_URL = "https://your-backend-domain.com"; // Update this with your actual backend URL
 
 export { API_BASE_URL };
