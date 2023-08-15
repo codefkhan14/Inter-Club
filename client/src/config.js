d17dafaba6b92812267e3e4d79048f9505596da4
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-08-15 14:15:04 +0530

    add env file

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://interclub.onrender.com";
+const API_BASE_URL = process.env.API_BASE_URL;
 // const API_BASE_URL = "http://localhost:5000";
 // const API_BASE_URL = "https://your-backend-domain.com"; // Update this with your actual backend URL
 
