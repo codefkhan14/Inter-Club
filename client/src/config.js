a544003c805f84414cbc322123166e69e9013368
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-08-15 15:23:46 +0530

    fix bugs

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = process.env.API_BASE_URL;
-// const API_BASE_URL = "http://localhost:5000";
-// const API_BASE_URL = "https://your-backend-domain.com"; // Update this with your actual backend URL
+
+
+const API_BASE_URL = process.env.REACT_APP_API_KEY;
 
 export { API_BASE_URL };
