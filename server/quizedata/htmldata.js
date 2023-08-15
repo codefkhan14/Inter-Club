672c9dc97b26160a50a25f0b9c7df7244b30dd16
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-08-15 13:45:08 +0530

    add ques

--- server/quizedata/htmldata.js
+++ server/quizedata/htmldata.js
@@ -17,6 +17,12 @@ const HtmlData = [
       correctAnswer: 'Mars',
       explanation: "djlsdjfdsljohfwofhdsldslkjdslfjdslfj;l",
     },
+    {
+      question: 'Who is md furkan?',
+      options: ['Mars', 'Venus', 'Jupiter', 'hwo'],
+      correctAnswer: 'Mars',
+      explanation: "djlsdjfdsljohfwofhdsldslkjdslfjdslfj;l",
+    },
     // More questions...
   ];
   
