6eab64d04898937bcf25841d5a4300c2d8080e4e
Mohammed Frukan <furkanrangrej200@gmail.com>
2023-08-27 12:57:27 +0530

    some minor changes

--- client/tempCodeRunnerFile.javascript
+++
@@ -1,3 +0,0 @@
-let a = [1,2,3,4];
-let b = [1,2,3,4];
-console.log(JSON.stringify(a)==JSON.stringify(b));
---
+++ server/quizedata/reactdata.js
@@ -0,0 +1,47 @@
+const ReactData = [
+  {
+    question: "1. What is Babel?",
+    options: [
+      "JavaScript compiler",
+      "JavaScript interpreter",
+      "JavaScript transpiler",
+      "None of the above",
+    ],
+    correctAnswer: "JavaScript compiler",
+    explanation: "Babel is a JavaScript compiler",
+  },
+  {
+    question: "2. How many elements can a valid react component return?",
+    options: ["1", "2", "3", "4"],
+    correctAnswer: "1",
+    explanation: "React component return only 1 element",
+  },
+  {
+    question: "3. A state in React.js is also known as?",
+    options: [
+      "The internal storage of the component",
+      "External storage of the component",
+      "Permanent Storage",
+      "All of the above",
+    ],
+    correctAnswer: "The internal storage of the component",
+    explanation:
+      " A state in React.js is also known as the internal storage of the component",
+  },
+  {
+    question: "4. In MVC, what does React.js act as?",
+    options: ["Modal", "Controller", "View", "Middleware"],
+    correctAnswer: "View",
+    explanation: "n MVC, React.js acts as the View",
+  },
+  {
+    question: "5. Among The following options, choose the one which helps react for keeping their data uni-directional?",
+    options: ["DOM", "Flux", "JSX", "Props"],
+    correctAnswer: "Flux",
+    explanation: " FLux helps react for keeping their data uni-directional",
+  },
+  
+  // More questions...
+];
+
+module.exports = ReactData;
